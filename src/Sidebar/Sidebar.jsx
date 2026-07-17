432e0f6c02261314ce6d334ec3aae15985488ae9
Joseph Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-07-17 21:04:58 +0300

    feat: compose sidebar with category, price, and color filters

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -6,9 +6,6 @@ const Sidebar = ({ handleChange }) => {
   return (
     <>
      <section className="sidebar">
-       <div className="logo-container">
-         <h1>🛒</h1>
-       </div>
        <Category handleChange={handleChange} />
        <Price handleChange={handleChange} />
        <Color handleChange={handleChange} />
