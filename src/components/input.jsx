f8393fb123fad094bf3012aaab122459a7ce1202
Joseph Wachira <wachira.wangui2023@students.jkuat.ac.ke>
2026-07-17 21:04:13 +0300

    feat: create reusable input component with custom radio styles

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,12 +1,11 @@
-
-
-const Input = ({handleChange, name, value, title, color}) => {
+import "./input.css"
+const Input = ({handleChange, name, value, title, color, className=""}) => {
   return (
     <div>
       <label className="sidebar-label-container">
-          <input type="radio" name={name} value={value} onChange={handleChange} />
-      <span style={{ color }}>{title}</span>
+          <input type="radio" name={name} value={value} onChange={handleChange} style={{ backgroundColor: color }} className={className}/>
         </label>
+        <span style={{ color }}>{title}</span>
     </div>
   )
 }
